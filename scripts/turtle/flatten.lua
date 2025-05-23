b6d365df001c5d2a3766dc4326beb826a4648202
DoubleTroublePy <doubletroublepy@gmail.com>
2025-05-23 22:17:18 +0200

    .

--- scripts/turtle/flatten.lua
+++ scripts/turtle/flatten.lua
@@ -1,3 +1,18 @@
+function refuel()
+  while true do
+    for i=1, 16 do
+      turtle.select(i)
+      -- try to refuel whit every item in the turtle if stop when encunters a 
+      -- valid one
+      if turtle.refuel(1) then return end
+    end
+    print("... waiting fuel ...")
+    print("... press any key ...")
+    io.read()
+    os.sleep(.1)
+  end
+end
+
 function forward()
   if turtle.getFuelLevel() < 10 then
     turtle.refuel(1)
@@ -5,36 +20,58 @@ function forward()
   turtle.forward()
 end
 
-function dig_move()
+function place_beind(item)
+  while true do
+    for i=1, 16 do
+      data = turtle.getItemDetail(i)
+      if data.name == item then
+        select(i)
+        turtle.turnRight()
+        turtle.turnRight()
+        turtle.place()
+        turtle.turnRight()
+        turtle.turnRight()
+        return
+      end
+    end
+    print("... waiting " .. item .. " ...")
+    print("... press any key ...")
+    io.read()
+    os.sleep(.1)
+  end
+end
+
+function dig_move(item, replace)
+  replace = replace or false
   if turtle.detect() then
     turtle.dig()
+    place_beind(item)
   end
   forward()
 end
 
 -- main loop
-print("lenght: ")
-local lenght = io.read()-1
-
-print("width: ")
-local width = io.read()-1
-
-print("depth: ")
-local depth = io.read()
+-- TODO: give a prompt in case of an error
+-- XXX: no error checking 
+local lenght = tonumber(arg[1])-1
+local width = tonumber(arg[2])
+local depth = tonumber(arg[3])+1
 local side = true
+local replace = false
 
 for level=1, depth do
+  if level == depth then replace = true end
   for row=1, width do
     for line=1, lenght do
       dig_move()
     end
     if side then
       turtle.turnRight()
-      forward()
+      dig_move(replace)
       turtle.turnRight()
     else
       turtle.turnLeft()
-      forward()
+      dig_move(replace)
       turtle.turnLeft()
     end
     side = not side
@@ -42,6 +79,8 @@ for level=1, depth do
   if level ~= depth then
     turtle.digDown()
     turtle.down()
+    turtle.turnRight()
+    turtle.turnRight()
   end
 end
 

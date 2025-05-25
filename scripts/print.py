a684287b12b8d5a7a17a422718f6f7eb4e1f79e3
DoubleTroublePy <doubletroublepy@gmail.com>
2025-05-25 14:17:30 +0200

    .

--- scripts/print.py
+++ scripts/print.py
@@ -1,7 +1,9 @@
 import csv
+from matplotlib import animation
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.patches as mpatches
+from matplotlib.animation import FuncAnimation
 
 tmp = []
 data_csv = []
@@ -13,39 +15,47 @@ with open('./chunk.csv', newline='') as csvfile:
         #print(row)
 cmp = list(set(tmp))
 
-data = [], [], []
-for row in data_csv:
-    if int(row[3]) == 0:
-        data[0].append(int(row[1])) 
-        data[1].append(int(row[2]))
-        for i in range(len(cmp)):
-            if cmp[i] == row[0]:
-                data[2].append(i)
-                break
+fg, ax = plt.subplots()
 
-mxz = min(data[0])
-mxx = min(data[1])
-Mxz = max(data[0]) + abs(mxz)
-Mxx = max(data[1]) + abs(mxx)
+def update(slc):
+    data = [], [], []
+    for row in data_csv:
+        if int(row[3]) == slc:
+            data[0].append(int(row[1])) 
+            data[1].append(int(row[2]))
+            for i in range(len(cmp)):
+                if cmp[i] == row[0]:
+                    data[2].append(i)
+                    break
 
-matrix = mat = [[-1 for _ in range(Mxz+1)] for _ in range(Mxx+1)]
-for i in range(len(data[0])):
-    y = data[0][i] + abs(mxx)
-    x = data[1][i] + abs(mxz)
-    val = data[2][i]
-    matrix[x][y] = val
+    mxz = min(data[0])
+    mxx = min(data[1])
+    Mxz = max(data[0]) + abs(mxz)
+    Mxx = max(data[1]) + abs(mxx)
 
-px = plt.figure()
+    matrix = mat = [[-1 for _ in range(Mxz+1)] for _ in range(Mxx+1)]
+    for i in range(len(data[0])):
+        y = data[0][i] + abs(mxx)
+        x = data[1][i] + abs(mxz)
+        val = data[2][i]
+        matrix[x][y] = val
 
-px = plt.imshow(matrix, cmap='tab20c', interpolation='nearest', origin='lower')
+    px = plt.figure()
 
-# get the colors of the values, according to the 
-# colormap used by imshow
-colors = [ px.cmap(px.norm(value)) for value in range(len(cmp))]
-# create a patch (proxy artist) for every color 
-patches = [ mpatches.Patch(color=colors[i], label=cmp[i] ) for i in range(len(cmp)) ]
-# put those patched as legend-handles into the legend
-plt.legend(handles=patches, bbox_to_anchor=(1, 1), loc=2, borderaxespad=0. )
-plt.tight_layout()
-plt.show()
+    px = plt.imshow(matrix, cmap='tab20c', interpolation='nearest', origin='lower')
+    #px.imshow(matrix, cmap='tab20c', interpolation='nearest', origin='lower')
+
+    # get the colors of the values, according to the 
+    # colormap used by imshow
+    colors = [ px.cmap(px.norm(value)) for value in range(len(cmp))]
+    # create a patch (proxy artist) for every color 
+    patches = [ mpatches.Patch(color=colors[i], label=cmp[i] ) for i in range(len(cmp)) ]
+    # put those patched as legend-handles into the legend
+    plt.legend(handles=patches, bbox_to_anchor=(1, 1), loc=2, borderaxespad=0. )
+    plt.tight_layout()
+    plt.show()
+
+
+for i in range(10):
+    update(i)
 
